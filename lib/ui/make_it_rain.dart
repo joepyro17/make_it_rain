cf9e7f047393259b92653d09bb5aa08f9ef8c141
Sithixay Douangchak <>
2018-09-02 12:40:15 +0700

    add project

---
+++ lib/ui/make_it_rain.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MakeItRain extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+
+    return new MakeItRainState();
+      }
+    
+}
+    
+class MakeItRainState extends State <MakeItRain> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Make It Rain!'),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: new Container(
+        child: new Column(
+           children: <Widget>[
+             new Center(
+               child: new Text("Get Rich!",
+                 style: new TextStyle(
+                   color: Colors.blueGrey,
+                   fontWeight: FontWeight.w400,
+                   fontSize: 29.9
+                 ),
+
+               )
+             )
+           ],
+        )
+      ),
+    );
+  }
+
+}
